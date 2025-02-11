6497c291dd5ef5a86fe55aedebab76654911af57
Bishal <bishal777giri@gmail.com>
2025-02-11 11:48:01 +0545

    started project

---
+++ data_visualizer/main.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pygame.init()
+WIDTH,HEIGHT=1200,1080
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+
+WHITE=(255,255,255)
+BLACK=(0,0,0)
