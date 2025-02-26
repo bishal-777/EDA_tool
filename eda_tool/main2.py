566359723c07b40521d25a6be76b6c79fbf1f5bb
Bishal <bishal777giri@gmail.com>
2025-02-26 23:14:45 +0545

    added functions for both univariate and bivariate analysis

--- eda_tool/main2.py
+++ eda_tool/main2.py
@@ -1,5 +1,7 @@
 import pygame
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tkinter import filedialog
 
 pygame.init()
@@ -23,6 +25,44 @@ def load_dataset():
      if file_path:
           return pd.read_csv(file_path)
      return None
+
+def histogram(df,feature):
+    plt.figure(figsize=(8,4))
+    plt.hist(df[feature],bins=20)
+    plt.title(f"Histogram of {feature}")
+    plt.show()
+
+def boxplot(df,feature):
+     plt.figure(figsize=(8,8))
+     sns.boxplot(df[feature])
+     plt.title(f"Boxplot of {feature}")
+     plt.show()
+
+def barchart(df,feature):
+     plt.figure(figsize=(8,4))
+     sns.countplot(x=df[feature])
+     plt.title(f"Countplot of {feature}")
+     plt.show()
+
+def piechart(df,feature):
+     counts=df[feature].value_counts()
+     plt.figure(figsize=(8,4))
+     plt.pie(counts,labels=counts.index)
+     plt.title(f"Pie Chart of {feature}")
+     plt.show()
+
+def scatterplot(df,feature1,feature2):
+     plt.figure(figsize=(8,4))
+     sns.scatterplot(x=df[feature1],y=df[feature2])
+     plt.title(f"Scatterplot of {feature1} and {feature2}")
+     plt.show()
+
+def pairplot(df,feature1,feature2):
+     plt.figure(figsize=(20,12))
+     sns.pairplot(data=df,vars=[feature1,feature2])
+     plt.title(f"Pairplot of {feature1} and{feature2}")
+     plt.show()
+
 
 running=True
 while running==True:
